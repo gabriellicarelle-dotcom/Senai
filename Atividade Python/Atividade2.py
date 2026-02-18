7579c652cb5813ae24095723e2c19b127588fbed
gabriellicarelle-dotcom <gabrielli_carelle@estudante.sesisenai.org.br>
2026-02-18 20:23:42 +0000

    update

---
+++ Atividade Python/Atividade2.py
@@ -0,0 +1,7 @@
+a = input("Insira um número: ")
+b = input("Insira o segundo número: ")
+print (a+b)
+
+a = float(input("Insira um número: "))
+b = float(input("Insira o segundo número: "))
+print (a+b)
