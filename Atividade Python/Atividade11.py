9af688da14e3ad80ea1c0b18cf9ee7eea85fd1c7
gabriellicarelle-dotcom <gabrielli_carelle@estudante.sesisenai.org.br>
2026-03-18 19:58:59 +0000

    update

--- Atividade Python/Atividade11.py
+++ Atividade Python/Atividade11.py
@@ -1,7 +1,41 @@
-# while continuar( continuar = true)
-# if 
-# else
-# elif
-# for
 
-0
+
+estoque = {}
+
+print ("Bem vindo ao sistema de gestão de estoque, desenvolvido por Gabrielli Carelle")
+
+while True:
+    operacao = input ("Deseja registrar a entrada e saída de produtos? (digite 'entrada' ou 'saída') ou 'sair' ").lower()
+    
+    if operacao not in ['entrada','saída', 'sair']:
+        print ("Operação inválida.")
+        continue
+    
+    if operacao == 'sair':
+        break
+    produto = input ("Nome do produto: ").strip()
+    qtd = int(input("Quantidade: ")) 
+
+    if operacao == 'entrada':
+        estoque[produto] = estoque.get(produto, 0) + qtd
+    elif operacao == 'saída':
+        if estoque.get(produto, 0) >= qtd:
+            estoque[produto] -= qtd 
+        else:
+            print ("Erro: produto inexistente ou estoque insuficiente.")
+
+print ("\n ---Estoque Final---")
+for p, q in estoque.items():
+        print (f"{p} : {q}")
+
+
+
+
+# Inicia o programa
+
+
+
+
+
+
+
