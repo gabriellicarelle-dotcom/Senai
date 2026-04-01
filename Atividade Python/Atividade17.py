1b3ed0b2d160562902e26673b78ca56952a0fe24
gabriellicarelle-dotcom <gabrielli_carelle@estudante.sesisenai.org.br>
2026-04-01 17:37:55 +0000

    update

--- Atividade Python/Atividade17.py
+++ Atividade Python/Atividade17.py
@@ -1,20 +1,21 @@
-# A. Mensagem de boas-vindas
+# Mensagem de boas-vindas
 print("Bem-vindo ao sistema de controle de acesso!")
 print("Desenvolvido por Gabrielli\n")
 
-# Pergunta o dia da semana
+# Pergunta o dia da semana.
+# lower quer dizer que se o usúario digitar com letra maiúscula ou minúscula  o programa vai entender igual
 dia = input("Digite o dia da semana: ").lower()
 
-# B. Tipo de usuário
+# Tipo de usuário
 tipo = input("Você é membro ou visitante? ").lower()
 
 # Se for visitante, pede o tempo de permanência
+# == é um operador de comparação
 if tipo == "visitante":
     horas = int(input("Quantas horas deseja permanecer? "))
 else:
-    horas = 0  # membro não precisa informar horas
+    horas = 0  # membro não precisa informar as horas
 
-# C e D. Verificação das regras com if, elif e else
 
 # Verifica se é fim de semana
 if dia == "sabado" or dia == "domingo":
@@ -31,7 +32,7 @@ else:
         print("Acesso permitido: membros têm acesso ilimitado em horário comercial.")
     
     elif tipo == "visitante":
-        # E. Verifica limite de horas
+        # Verifica limite de horas
         if horas <= 4:
             print("Acesso permitido: tempo dentro do limite de 4 horas.")
         else:
@@ -40,4 +41,3 @@ else:
     else:
         print("Tipo de usuário inválido.")
 
-# F. Mensagens já indicam se foi permitido ou não e o motivo
